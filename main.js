bd771daffe7ac0b32ba4a650f4cd01f098116eb9
Sunita <54287247+sunitase@users.noreply.github.com>
2019-09-08 14:58:51 -0700

    final commit

--- main.js
+++ main.js
@@ -21,44 +21,22 @@ for(var i = 0; i < planets.length; i++) {
   //var select = document.getElementById("planets"); 
     var opt = document.createElement('option');
     opt.innerHTML = planets[i][0];
-    opt.value = planets[i][1];
+    opt.value = planets[i][0];
     select.appendChild(opt);
-}
+      }
 
-
-
- function calculateWeight(userWeight, planetName) {
-  var userWeight = document.getElementById("user-weight").value;
- var planetName = planets[select.selectedIndex][0];
+function calculateWeight(userWeight, planetName) {
   for(var i = 0; i < planets.length; i++){
     if(planetName == planets[i][0]) {
-      var result = userWeight * planets[i][1];
-      return result;
+    var result = userWeight * planets[i][1]; 
+    return result;
     }
   }
-
-  //var selectedPlanetIndex = document.getElementById("planets").selectedIndex;
-  //var selectedPlanet = selectedPlanetIndex.document.getElementById("planets").options[index].text);
-  //var index = document.getElementById(select1).selectedIndex;
-  //var selectedText = selectedPlanet.options[selectedPlanet.selectedIndex].text;
-//var result = userWeight * planetName;
-//console.log(selectedPlanet);
-//console.log(result);
-  //return result;
- }
- 
-
+}
   function handleClickEvent(e){
     var userWeight = document.getElementById("user-weight").value;
-    
     var planetName = planets[select.selectedIndex][0];
-  
    var result = calculateWeight(userWeight, planetName);
-   // document.getElementById("calculate-button").addEventListener("click", calculateWeight());
-    document.getElementById("output").innerHTML = 'If you were on ' +planetName+', you would weigh '+result+'lbs!';
-  
+  document.getElementById("output").innerHTML = 'If you were on ' +planetName+', you would weigh '+result+'lbs!';
   } 
-  
  
-
-  
